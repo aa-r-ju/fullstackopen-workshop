11b63dfa3e22b85e4dfdb33d1d50d2a9a8cd42bb
aa-r-ju <aarju3447@gmail.com>
2023-08-23 09:27:25 +0545

    hello

--- part3/server/mongo.js
+++ part3/server/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length<3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://sharmila:${password}@cluster0.cczhdfv.mongodb.net/tej?retryWrites=true&w=majority`
+const url = `mongodb+srv://aarju3447:${password}@cluster0.phks67i.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
